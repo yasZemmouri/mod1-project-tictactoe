39351f0ec982232c253655d0f08e698e824d0c85
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2021-12-25 11:49:33 -0500

    SP: player can write on board

--- algo.js
+++ algo.js
@@ -1,1 +1,46 @@
 "use strict";
+
+//throw error???
+//=================Reference Elements=============================
+//Cases Elements
+//Do i really need to put id on every case in html???
+const caseEl = Array.from(
+  document.querySelectorAll(`#container1
+ .case`)
+);
+const pEl = document.getElementsByTagName("p");
+const gameOverEl = document.getElementById("gameOver");
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//========================== Function Declarations =======================//
+const player1move = function () {
+  this.textContent = "X";
+};
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//==========================Event Listners=============================
+//Click on cases Listners
+
+caseEl[0].addEventListener("click", player1move);
+caseEl[1].addEventListener("click", player1move);
+caseEl[2].addEventListener("click", player1move);
+caseEl[3].addEventListener("click", player1move);
+caseEl[4].addEventListener("click", player1move);
+caseEl[5].addEventListener("click", player1move);
+caseEl[6].addEventListener("click", player1move);
+caseEl[7].addEventListener("click", player1move);
+caseEl[8].addEventListener("click", player1move);
+
+// //ways to declare array
+// //ways to declare functions
+
+// //function player win.
+// //function initial state.
+// //function move.
+// //use class as arrays better than using id.
+
+//study this
+
+//shortcut for the for loop
+//different color for circle
+//prevent player from playing before computer make move
+//...Operator
+//pass array to a funciton???
