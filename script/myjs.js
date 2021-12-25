c6dc214eddfd537e6f792c5c4da6cc7cf978f98f
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2021-12-25 10:38:59 -0500

    using two pages for two versions one single player and one multiplayer to reduce complixity

--- script/myjs.js
+++ script/myjs.js
@@ -1,32 +1,19 @@
 "use strict";
 
-// const cases = Array.from(document.querySelectorAll(".case"));
-//Access Elements
-const caseOneEl = document.getElementsByClassName("caseOne");
-// console.log(caseOneEl);
-const cOne0El = caseOneEl[0];
-const cOne1El = caseOneEl[1];
-const cOne2El = caseOneEl[2];
-const cOne3El = caseOneEl[3];
-const cOne4El = caseOneEl[4];
-const cOne5El = caseOneEl[5];
-const cOne6El = caseOneEl[6];
-const cOne7El = caseOneEl[7];
-const cOne8El = caseOneEl[8];
-cOne1El.textContent = "O";
-// console.log(cOne8El);
-const caseTwoEl = document.getElementsByClassName("caseTwo");
-const cTwo0El = caseTwoEl[0];
-const cTwo1El = caseTwoEl[1];
-const cTwo2El = caseTwoEl[2];
-const cTwo3El = caseTwoEl[3];
-const cTwo4El = caseTwoEl[4];
-const cTwo5El = caseTwoEl[5];
-const cTwo6El = caseTwoEl[6];
-const cTwo7El = caseTwoEl[7];
-const cTwo8El = caseTwoEl[8];
+// console.log(c8El);
+// const caseEl = document.getElementsByClassName("case");
+// console.log("global caseEl: " + caseEl);
+// const c0El = caseEl[0];
+// const c1El = caseEl[1];
+// const c2El = caseEl[2];
+// const c3El = caseEl[3];
+// const c4El = caseEl[4];
+// const c5El = caseEl[5];
+// const c6El = caseEl[6];
+// const c7El = caseEl[7];
+// const c8El = caseEl[8];
 
-const startEl = document.getElementById("start");
+// const startEl = document.getElementById("start");
 
 class Player {
   constructor(name, signe) {
@@ -36,20 +23,57 @@ class Player {
 }
 class Game {
   constructor(
+    name
     //global Variables
 
-    casesLeft = 9,
-    turn = 1,
+    // casesLeft = 9,
+    // turn = 1,
     //boardSituation
-    boardSituation = new Array(9).fill(0),
+    // boardSituation = new Array(9).fill(0),
     //List of Empty Cases
-    casesLeftIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    // casesLeftIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8]
   ) {
     let self = this;
-    this.casesLeft = casesLeft;
-    this.turn = turn;
-    this.boardSituation = boardSituation;
-    this.casesLeftIndexes = casesLeftIndexes;
+    this.name = name;
+    //Access Elements
+    const caseEl = Array.from(
+      document.querySelectorAll(`#container${self.name} .case`)
+    );
+    // const caseEl = document.querySelectorAll(`${self.name} .case`);
+    // const caseEl = document.querySelectorAll(`#container1 .case`);
+    console.log(caseEl);
+    //------------------------------------------------------------------------------
+    const c0El = caseEl[0];
+    // console.log("caseEl[0] content: " + (caseEl[0].textContent = "OK"));
+    // console.log("c0El: " + c1El.textContent);
+    const c1El = caseEl[1];
+    console.log(c0El);
+    const c2El = caseEl[2];
+    const c3El = caseEl[3];
+    const c4El = caseEl[4];
+    const c5El = caseEl[5];
+    const c6El = caseEl[6];
+    const c7El = caseEl[7];
+    const c8El = caseEl[8];
+    // startEl.addEventListener("click", game1.start, false);
+    // event listners
+
+    // // console.log(c0El);
+    // // board2 event listners
+    // c0El.addEventListener("click", game2.player1move, false);
+    // c1El.addEventListener("click", game2.player1move, false);
+    // c2El.addEventListener("click", game2.player1move, false);
+    // c3El.addEventListener("click", game2.player1move, false);
+    // c4El.addEventListener("click", game2.player1move, false);
+    // c5El.addEventListener("click", game2.player1move, false);
+    // c6El.addEventListener("click", game2.player1move, false);
+    // c7El.addEventListener("click", game2.player1move, false);
+    // c8El.addEventListener("click", game2.player1move, false);
+    // c1El.textContent = "O";
+    this.casesLeft = 9;
+    this.turn = 1;
+    this.boardSituation = new Array(9).fill(0);
+    this.casesLeftIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     // this.player1move = this.player1move.bind(this);
     this.updateBoard = function (caseIndex) {
       self.boardSituation[caseIndex] = self.turn;
@@ -57,7 +81,7 @@ class Game {
       //tester
       let myvar = "";
       for (let i = 0; i < 9; i++) {
-        myvar = myvar + " " + boardSituation[i];
+        myvar = myvar + " " + self.boardSituation[i];
       }
       console.log("boardSituation: " + myvar);
     };
@@ -68,11 +92,20 @@ class Game {
       //if it finds return it's index.s
       let m = self.casesLeftIndexes.indexOf(caseIndex);
       //swap
-      [self.casesLeftIndexes[m], self.casesLeftIndexes[casesLeft - 1]] = [
-        self.casesLeftIndexes[casesLeft - 1],
+      [self.casesLeftIndexes[m], self.casesLeftIndexes[self.casesLeft - 1]] = [
+        self.casesLeftIndexes[self.casesLeft - 1],
         [self.casesLeftIndexes[m]],
       ];
       self.casesLeftIndexes.pop();
+      //   why not splice to remove the played element instead of swap and pop
+      //   var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+      // for( var i = 0; i < arr.length; i++){
+
+      //     if ( arr[i] === 5) {
+
+      //         arr.splice(i, 1);
+      //     }/
       //test swap
       let myvar = "";
       for (let i = 0; i < self.casesLeftIndexes.length; i++) {
@@ -80,6 +113,7 @@ class Game {
       }
       console.log("casesLeftIndexes: " + myvar);
     };
+
     self.checkWin = function () {
       console.log("checkWin says Hello!");
       let winner = 3 - self.turn; //since turns are inversed before making the call. we inverse them back
@@ -130,7 +164,7 @@ class Game {
 
     this.player1move = function () {
       if (self.turn === 1 && this.textContent === "") {
-        this.textContent = "M";
+        this.textContent = "X";
         let caseIndex = parseInt(this.id[1]);
         // console.log("caseIndex is: " + caseIndex);
         console.log("player 1 moved");
@@ -139,14 +173,26 @@ class Game {
         //--------------------------------------------------------------
         self.updateBoard(caseIndex); //before changing the turn
         self.updatecasesLeftIndexes(caseIndex);
-        self.turn = 2;
+        self.turn = 2; //removed for testing reasons
         self.checkWin(); //|| checkFullBoard(); //change turn to 0 if board full. should be after turn change
         self.player2move();
       }
     };
+    c0El.addEventListener("click", this.player1move, false);
+    c1El.addEventListener("click", this.player1move, false);
+    c2El.addEventListener("click", this.player1move, false);
+    c3El.addEventListener("click", this.player1move, false);
+    c4El.addEventListener("click", this.player1move, false);
+    c5El.addEventListener("click", this.player1move, false);
+    c6El.addEventListener("click", this.player1move, false);
+    c7El.addEventListener("click", this.player1move, false);
+    c8El.addEventListener("click", this.player1move, false);
+    c0El.addEventListener("click", function () {
+      c0El.style.backgroundColor = "red";
+    });
     this.player2move = function () {
       if (self.turn === 2) {
-        let caseIndex = null;
+        // let caseIndex = null;
         console.log("player 2 thinking");
         // if (level === 1) {
         //   caseIndex = level1();
@@ -156,13 +202,15 @@ class Game {
         //   caseIndex = level3();
         // }
         setTimeout(function () {
+          let caseRandom = Math.floor(Math.random() * self.casesLeft);
+          let caseIndex = self.casesLeftIndexes[caseRandom];
           console.log("player 2 moved");
           self.updateBoard(caseIndex); //before changing turn
           self.updatecasesLeftIndexes(caseIndex);
           caseEl[caseIndex].classList.add("stroke-txt");
           caseEl[caseIndex].textContent = "O";
           // turn = 0;//didn't work why??/
-          turn = 1;
+          self.turn = 1;
           self.checkWin(); // || checkFullBoard(); //must be executed after turn = 1; or we can use a condition. or maybe turn = check@in()||checkFullBoard()
         }, 600); //doesn't stop player 1 from playing
 
@@ -170,60 +218,49 @@ class Game {
         // // gameOver();
       }
     };
+    this.greating = function () {
+      console.log(`game${self.name} is initialized!`);
+    };
+    //randomely select which player moves first
+    this.playerToStart = function () {
+      this.turn = Math.floor(Math.random() * 2 + 1);
+      console.log(`Player ${this.turn} goes first`);
+    };
   }
-  greating() {
-    return "is initialized!";
-  }
-
-  //randomely select which player moves first
-  // playerToStart = function () {
-  //   this.turn = Math.floor(Math.random() * 2 + 1);
-  //   console.log(`Player ${this.turn} goes first`);
-  // };
 }
+// console.log(document.querySelectorAll("#container1 .case"));
+const game1 = new Game(1);
+game1.greating();
 
-const game1 = new Game();
-console.log("game1 " + game1.greating());
-
-// game1.playerToStart();
-const game2 = new Game();
-console.log("game2 " + game2.greating());
+// // game1.playerToStart();
+const game2 = new Game(2);
+console.log(game2.name);
+game2.greating();
 // game2.playerToStart();
-// jeu.playerToStart();
+// // jeu.playerToStart();
 
-// const game = {
-//   casesLeft = 9,
-
-//   start: () => {
-//     console.log("We are starting a game!");
-//   },
-
-//   turn: 0,
-//   boardSituation: new Array(9).fill(0),
-// };
-
-startEl.addEventListener("click", game1.start, false);
-// bord1 event listners
-cOne0El.addEventListener("click", game1.player1move, false);
-cOne1El.addEventListener("click", game1.player1move, false);
-cOne2El.addEventListener("click", game1.player1move, false);
-cOne3El.addEventListener("click", game1.player1move, false);
-cOne4El.addEventListener("click", game1.player1move, false);
-cOne5El.addEventListener("click", game1.player1move, false);
-cOne6El.addEventListener("click", game1.player1move, false);
-cOne7El.addEventListener("click", game1.player1move, false);
-cOne8El.addEventListener("click", game1.player1move, false);
-cOne0El.addEventListener("click", function () {
-  cOne0El.style.backgroundColor = "red";
-});
-// console.log(cOne0El);
-// board2 event listners
-cTwo0El.addEventListener("click", game2.player1move, false);
-cTwo1El.addEventListener("click", game2.player1move, false);
-cTwo2El.addEventListener("click", game2.player1move, false);
-cTwo3El.addEventListener("click", game2.player1move, false);
-cTwo4El.addEventListener("click", game2.player1move, false);
-cTwo5El.addEventListener("click", game2.player1move, false);
-cTwo6El.addEventListener("click", game2.player1move, false);
-cTwo7El.addEventListener("click", game2.player1move, false);
-cTwo8El.addEventListener("click", game2.player1move, false);
+// startEl.addEventListener("click", game1.start, false);
+// // bord1 event listners
+// c0El.addEventListener("click", game1.player1move, false);
+// c1El.addEventListener("click", game1.player1move, false);
+// c2El.addEventListener("click", game1.player1move, false);
+// c3El.addEventListener("click", game1.player1move, false);
+// c4El.addEventListener("click", game1.player1move, false);
+// c5El.addEventListener("click", game1.player1move, false);
+// c6El.addEventListener("click", game1.player1move, false);
+// c7El.addEventListener("click", game1.player1move, false);
+// c8El.addEventListener("click", game1.player1move, false);
+// c0El.addEventListener("click", function () {
+//   c0El.style.backgroundColor = "red";
+// });
+// // console.log(c0El);
+// // board2 event listners
+// c0El.addEventListener("click", game2.player1move, false);
+// c1El.addEventListener("click", game2.player1move, false);
+// c2El.addEventListener("click", game2.player1move, false);
+// c3El.addEventListener("click", game2.player1move, false);
+// c4El.addEventListener("click", game2.player1move, false);
+// c5El.addEventListener("click", game2.player1move, false);
+// c6El.addEventListener("click", game2.player1move, false);
+// c7El.addEventListener("click", game2.player1move, false);
+// c8El.addEventListener("click", game2.player1move, false);
