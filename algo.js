a8dad8c613c3cf2d1334eec86eab77b4f5826888
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2021-12-25 19:34:18 -0500

    reset UI board

--- algo.js
+++ algo.js
@@ -9,11 +9,26 @@ const caseEl = Array.from(
 );
 const pEl = document.getElementsByTagName("p");
 const gameOverEl = document.getElementById("gameOver");
+const startEl = document.getElementById("start");
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//========================= Global Variables ============================
+let turn = 1;
 //========================== Function Declarations =======================//
+const reset = function () {
+  for (let i = 0; i < 9; i++) {
+    caseEl[i].querySelector("div").classList.remove("x");
+    caseEl[i].querySelector("div").classList.remove("o");
+    console.log("remove");
+  }
+};
 const player1move = function () {
   //   this.textContent = "X";
-  this.querySelector("div").classList.add("x");
+  if (
+    turn === 1 &&
+    !this.querySelector("div").classList.contains("o") &&
+    !this.querySelector("div").classList.contains("x")
+  )
+    this.querySelector("div").classList.add("x");
 };
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 //==========================Event Listners=============================
@@ -28,6 +43,7 @@ caseEl[5].addEventListener("click", player1move);
 caseEl[6].addEventListener("click", player1move);
 caseEl[7].addEventListener("click", player1move);
 caseEl[8].addEventListener("click", player1move);
+startEl.addEventListener("click", reset);
 
 // //ways to declare array
 // //ways to declare functions
